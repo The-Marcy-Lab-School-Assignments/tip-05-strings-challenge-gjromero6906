e20ed8662a4dc52ad5c592552768f10cfa0d9eb4
gjromero6906 <gjr6906@gmail.com>
2025-10-07 12:18:25 -0400

    own solution,first try

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ a new string where the first letter of each word is capitalized.
 */
 const capitalizeWords = (sentence) =>{
  //write your solution here
+ let newSentence ='';
+ let sentenceLenght=sentence.length;
+
+ for(let i =0;i<sentenceLenght;i++){
+    if(i === 0){
+        newSentence+=sentence[i].toUpperCase();
+    }else if(sentence[i]===' '){
+        newSentence+=` ${sentence[i+1].toUpperCase()}`;
+        i++;
+    }else{
+        newSentence+=sentence[i];
+    }
+ }
+ return newSentence;
 }
 
-capitalizeWords("hello world from javascript");
+console.log(capitalizeWords("hello world from javascript"));
 // → "Hello World From Javascript"
 
-capitalizeWords("javaScript");
+console.log(capitalizeWords("javaScript"));
 // → "JavaScript"
